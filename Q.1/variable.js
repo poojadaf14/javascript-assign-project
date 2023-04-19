a51fa72bfca236fc8c3911779ced00ff3f7068cd
Pooja <poojadaf878@gmail.com>
2023-04-19 14:07:25 +0530

    Q.1 javascript

---
+++ Q.1/variable.js
@@ -0,0 +1,12 @@
+let x= 10;
+var y = 20;
+const z = 'pooja';
+
+//let, var, const- are three ways to declare variable.
+
+//data types
+
+
+//1. numbers
+let num= [1,2,3,5,6,7,8];
+console.log(num);
