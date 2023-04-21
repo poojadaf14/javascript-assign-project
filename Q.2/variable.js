930f7c24e53845c8f64db13f89690a8880cdfd74
Pooja <poojadaf878@gmail.com>
2023-04-21 15:30:13 +0530

    interpolation method

---
+++ Q.2/variable.js
@@ -0,0 +1,4 @@
+function myInfo(fname, lname, maritalstatus, age, country) {
+    return `My name is ${fname} ${lname}, marital status: ${maritalstatus}, my age is${age}, ${country} is my favorite country.`; 
+}
+console.log(myInfo("Pooja", "Daf", "Single", 22, "India"));
