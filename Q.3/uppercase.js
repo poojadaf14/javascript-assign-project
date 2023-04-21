019d0164108f26906bc299ae9675242663b26859
Pooja <poojadaf878@gmail.com>
2023-04-21 15:30:43 +0530

    touppercase method

---
+++ Q.3/uppercase.js
@@ -0,0 +1,21 @@
+let ABCDE = ['apple', 'ball', 'cat', 'elephant', 'dog'];
+
+let Upperfruit = [];
+
+for (const value of ABCDE) {
+    Upperfruit.push(value. toUpperCase());
+}
+
+console.log(Upperfruit);
+ 
+//same we can creat lowercase
+
+let fruit = ["APPLE"," MANGO", "BANANA", "WATERMELON"];
+
+let lowefruit = [];
+
+for (const size of fruit) {
+    lowefruit.push(size.toLowerCase());
+}
+
+console.log(lowefruit);
