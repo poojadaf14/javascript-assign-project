d780e5bb92c3c19d7affc327afcf652926d81d15
Pooja <poojadaf878@gmail.com>
2023-04-24 00:19:02 +0530

    includes condition

---
+++ Q.4/include.js
@@ -0,0 +1,11 @@
+ 
+ const arr = ["html", "css", "node", "react", "script", "javascript"];
+
+ const lang = arr.includes("script");
+ console.log(lang);
+
+
+ const arr1 = ["html", "css", "node", "react",];
+
+ const language = arr1.includes("script");
+ console.log(language);
