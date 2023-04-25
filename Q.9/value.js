f918ccca7b42a19af65a6c02ed5db6663f20c3f1
Pooja <poojadaf878@gmail.com>
2023-04-25 20:05:39 +0530

    value.js

---
+++ Q.9/value.js
@@ -0,0 +1,22 @@
+
+//boolean values
+
+//first three expression of getting value true,
+
+let isLoggedInfromgoggle = true;
+let isLoggedInfacebook = true;
+let isLoggedInfromEmail = true;
+
+console.log(isLoggedInfromgoggle);
+console.log(isLoggedInfacebook);
+console.log(isLoggedInfromEmail);
+
+//other three expression of getting value false,
+
+let isLoggedInfrominsta = false;
+let isLoggedInfacegmail = false;
+let isLoggedInfromapple = false;
+
+console.log(isLoggedInfrominsta);
+console.log(isLoggedInfacegmail);
+console.log(isLoggedInfromapple);
