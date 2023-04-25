476e7bb359ee498148e8dddbf11c08af05656904
Pooja <poojadaf878@gmail.com>
2023-04-25 20:05:09 +0530

    lastindex

---
+++ Q.7/lastindex.js
@@ -0,0 +1,9 @@
+
+const arr1 = ["pi", "dom", "js",  "pw skills", "jquery", "pw skills"];
+
+
+const arr2 = arr1.lastIndexOf("pw skills");
+const arr3 = arr1.lastIndexOf("pw skills",4);
+
+console.log(arr2);
+console.log(arr3);
