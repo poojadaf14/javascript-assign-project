761e2d605eb9c51e663192daabc455494c01fde5
Pooja <poojadaf878@gmail.com>
2023-04-26 21:05:12 +0530

    values.js

---
+++ Q.10/values.js
@@ -0,0 +1,79 @@
+if (4>3) {
+    console.log("the value is: true");
+} else{
+    console.log("the value is: false");
+}
+
+if (4>=3) {
+    console.log("true");
+} else{
+    console.log("false");
+}
+
+if (4<3) {
+    console.log("true");
+} else{
+    console.log("false");
+}
+
+if ( 4 <= 3) {
+    console.log("the value is: true");
+} else{
+    console.log("the value is: false");
+}
+
+if ( 4 == 4) {
+    console.log("the value is: true");
+} else{
+    console.log("the value is: false");
+}
+
+
+
+if (4 === 4 ) {
+    console.log("the value is: true");
+} else{
+    console.log("the value is: false");
+}
+
+if (4 != 4) {
+    console.log("true");
+} else{
+    console.log("false");
+}
+
+if (4 !== 4) {
+    console.log("true");
+} else{
+    console.log("false");
+}
+
+if (4 == '4') {
+    console.log("absolutely correct");
+} else{
+    console.log("not correct");
+}
+
+if (4 != '4') {
+    console.log("true");
+} else{
+    console.log("false");
+}
+
+
+if (4 === '4') {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+let arr1= "python";
+
+console.log(arr1.length);
+
+ arr1.lenght = "" ?console.log(true) :console.log(false);
+
+
+ let arr2 = "jargon";
+ console.log(arr2.length);;
+ arr2.length = "" ? console.log(true): console.log(false);
