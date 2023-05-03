fac2b0a3bd9882589c4205a3253b211d56f28890
Pooja <poojadaf878@gmail.com>
2023-05-03 21:48:32 +0530

    array

---
+++ Q.22/array.js
@@ -0,0 +1,12 @@
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+
+const arr= ages.sort();
+console.log(arr);
+
+const minage = Math.min(...arr);
+
+console.log(minage);
+
+const maxage = Math.max(...arr);
+console.log(maxage);
+
