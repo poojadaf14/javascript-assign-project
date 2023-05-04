2761ba72140ac6751602c4a34c8e182359db8eec
Pooja <poojadaf878@gmail.com>
2023-05-04 16:52:32 +0530

    array.js

--- Q.22/array.js
+++ Q.22/array.js
@@ -10,3 +10,35 @@ console.log(minage);
 const maxage = Math.max(...arr);
 console.log(maxage);
 
+
+const median = ages [Math.floor (ages.length/2)];
+console.log(median);
+
+//for average
+var sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+console.log(sum);
+  
+const average= (sum/arr.length);
+console.log(average);
+  
+
+//ranges
+const range1 = arr.indexOf(19);
+const range2 = arr.lastIndexOf(26);
+
+const range = (range2 - range1);
+console.log(range);
+
+//abs method
+const num1= console.log(minage/arr.length); 
+const num2= console.log(maxage/arr.length);
+
+console.log(Math.abs(2.6-1.9));
+if (1.9<2.6) {
+    console.log(Math.abs(2.6-1.9));
+} else {
+    console.log(Math.abs(1.9-2.6));
+}
